0150029e710cd5c3c1e192a0a3c7ea047f0bccd3
pierre8237 <34560968+pierre8237@users.noreply.github.com>
2020-10-15 10:29:37 -0400

    added static route to "/index  in routes/view.js  Including the assignment README

--- Develop/routes/view.js
+++ Develop/routes/view.js
@@ -8,5 +8,8 @@ router.get("/exercise", (req, res) => {
 router.get("/stats", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
-
+//////////////////////////////////////////////
+router.get("/index", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
 module.exports = router;
